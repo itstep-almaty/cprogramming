28c05b058dd7174e311473461153d3e967415137
Konstantin Latuta <konstantin.latuta@sdu.edu.kz>
2018-09-06 21:54:52 +0600

    Create example02.cpp

---
+++ module08/example02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	char s[10000]="";
+	char line[10000];
+
+	while (cin.getline(line, 10000)) {
+		strcat_s(s, line);
+		strcat_s(s, "\n");
+	}
+	
+	bool flag = true;
+	int len = strlen(s);
+	for (int i = 0; i < len; i++) {
+		if (isalpha(s[i]) && flag) {
+			if (islower(s[i])) s[i] = toupper(s[i]);
+			flag = false;
+		}
+		else
+			if (s[i] == '.') flag = true;
+	}
+	cout << s << endl;
+}
