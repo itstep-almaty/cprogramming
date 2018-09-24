9f2ef692b46cc8cbcc1294b2cd97f917eeed5526
Konstantin Latuta <konstantin.latuta@sdu.edu.kz>
2018-09-24 20:39:40 +0600

    Update pbms.cpp

--- module10/pbms.cpp
+++ module10/pbms.cpp
@@ -1,3 +1,12 @@
+// + load
+// + save
+// + create
+// + exit
+// add
+// remove
+// search
+// sort
+
 #include <fstream>
 #include <iostream>
 using namespace std;
@@ -8,14 +17,20 @@ struct Contact { // структура контакта
 }; 
 
 Contact* book; // динамический массив контактов
-int n;  // кол-во контактов в памяти
+int n=0;  // кол-во контактов в памяти
+int m=0;
 char bookname[30]; // текущее имя файла
+bool saved = true;
 
 void load() {
 	ifstream fin(bookname);
 	fin >> n;
-	book = new Contact[n];
+	
+	if (m != 0) delete[] book;
 
+	m = n + 5;
+	book = new Contact[m];
+	
 	for (int i = 0; i < n; i++) {
 		fin >> book[i].name >> book[i].phone;
 	}
@@ -33,11 +48,29 @@ void save() {
 	fout.close();
 }
 
+void create() {
+	m = 10;
+	n = 0;
+	book = new Contact[m];
+	cout << bookname << " was created." << endl;
+}
+
+void show() {
+	cout << bookname << (saved ? " saved" : " not saved") << endl;
+	for(int i = 0; i < n; i++)
+	{
+		cout << book[i].name << " " << book[i].phone << endl;
+	}
+}
 int main() {
 	char com[20];
 	cout << "Welcome to PhoneBook Management System" << endl;
 	cout << "\nEnter your command: ";
 	while (cin >> com) {
+		if (strcmp(com, "create") == 0) {
+			strcpy_s(bookname,"noname_book.txt");
+			create();
+		}
 		if (strcmp(com, "load")==0) {
 			cin >> bookname;
 			load();
@@ -47,6 +80,13 @@ int main() {
 		}
 		if (strcmp(com, "save")==0) {
 			save();				
+		}
+		if (strcmp(com, "saveas") == 0) {
+			cin >> bookname;
+			save();
+		}
+		if (strcmp(com, "show") == 0) {
+			show();
 		}
 		cout << "\nEnter your command: ";
 	}
