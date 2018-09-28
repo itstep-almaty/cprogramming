cb61a1aecb5fc4893dbbd3017da296e830108f1a
Konstantin Latuta <konstantin.latuta@sdu.edu.kz>
2018-09-28 20:17:49 +0600

    Update rat.cpp

--- module10/rat.cpp
+++ module10/rat.cpp
@@ -44,10 +44,39 @@ void print(Rational r, char del='\n') {
 	cout << r.n << "/" << r.m << del;
 }
 
+
+Rational add(Rational r1, Rational r2) {
+	Rational r3;
+	r3.m = r1.m*r2.m;
+	r3.n = r1.n*r2.m + r2.n*r1.m;
+	return r3;
+}
+Rational add(Rational r, int n) {
+	return add(r, newRational(n, 1));
+}
+Rational add(int n, Rational r) {
+	return add(newRational(n, 1),r);
+}
+
+void read(Rational &r) {
+	cin >> r.n;
+	cin.ignore();
+	cin >> r.m;
+}
+
 int main() {
 	Rational r1 = newRational(2,4);
 	Rational r2 = newRational(2,3);
 
 	print(r1);
 	print(r2);
+
+	Rational r3 = add(r1, r2);
+	print(r3);
+
+	print(add(5, r2));
+	print(add(r2, 5));
+
+	read(r3);
+	print(r3);
 }
